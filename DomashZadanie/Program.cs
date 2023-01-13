9e53d37440695c0c3efed2e106b265dc3f6a54d8
NikitsinIlya <JamsN87@gmail.com>
2023-01-13 15:25:27 +0300

    Task1 Done

--- DomashZadanie/Program.cs
+++ DomashZadanie/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 1
+// Напиши программу, котрая на вход принимает 2 числа и выдает, какое число больше, а какое меньше
+/*
+Console.Clear();
+
+Console.Write("Введите 1-e число : ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-e число : ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1} , min = {number2}");
+}
+else
+{
+    Console.WriteLine($"max = {number2} , min = {number1}");
+}
+
+*/
+
+
