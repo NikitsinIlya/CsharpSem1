b7b04db893f84cda3f1298ca3c116a5925b0bd30
NikitsinIlya <JamsN87@gmail.com>
2023-01-13 14:48:14 +0300

    Seminar programm

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.Clear();
+Console.Write();
+Console.WriteLine($"{a}, {b}"); // - интерполяция вывод самих данных
+
+int n = Convert.ToInt32(Console.ReadLine()); // перевод чисел из строки в цифры
+int n = int.Parse(Console.ReadLine());  // перевод чисел из строки в цифры
+
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine(m);
+else if (n < m)
+    Console.WriteLine(m);
+else 
+    Console.WriteLine("=");
+    */
