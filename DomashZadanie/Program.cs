98347f361e85aabe16d0d074bbf79dcbe000dd45
NikitsinIlya <JamsN87@gmail.com>
2023-01-13 16:03:46 +0300

    Task2 Done

--- DomashZadanie/Program.cs
+++ DomashZadanie/Program.cs
@@ -1,4 +1,5 @@
-﻿// Task 1
+﻿Console.Clear();
+// Task 1
 // Напиши программу, котрая на вход принимает 2 числа и выдает, какое число больше, а какое меньше
 /*
 Console.Clear();
@@ -18,4 +19,26 @@ else
 
 */
 
+//Task 2
+// Напиши программу, которая принемает на вход 3 числа и выдает максимальное из этих 3 чисел.
+/*
 
+Console.Write("Введите 1-е число: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+    int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (max <= number2)
+    {
+        max = number2;
+    }
+else if (max <= number3)
+{
+    max = number3;
+}  
+Console.WriteLine($"Максимальное число из {number1} , {number2} , {number3} Будет {max}");
+
+*/
